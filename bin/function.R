7d9816118bc4ae91b6f49ec6538e7f1cc5300810
shuangat <41887827+shuangat@users.noreply.github.com>
2019-01-14 17:35:44 +0800

    Update function.R

--- bin/function.R
+++ bin/function.R
@@ -1,67 +1,66 @@
-library(gProfileR)
-library(data.table)
-
-calculateCorrelation <- function(file) {
-  if (file.exists(file) == TRUE) {
-    data <- read.table(file, sep=",", header = T, row.names = 1)
-    
-    n.coding <- nrow(subset(data,info == "coding")) #i 
-    n.lnc <- nrow(subset(data, info == "lnc"))   #j
-    
-    data <- data[,-1]
-    data<-data.frame(cbind(data[,1],data.frame( log2(as.matrix(data[,-1]) +0.1) )))
-    names(data)[1] <- "gene"
-    
-    coding <- data[1:n.coding,]
-    lnc <- data[(n.coding+1):(n.coding+n.lnc),]
-    
-    pearson <- cor(t(data[,-1]))
-    pearson <- pearson[1:n.coding,(n.coding+1):(n.coding+n.lnc)]
-    pearson <- data.frame(pearson)
-    
-    correlation <- cbind( coding[,1], data.frame(abs(pearson)) )
-    names(correlation)[1] <- "gene"
-    
-    coding.info <- cbind(row.names(coding),data.frame(coding[,1]) ) 
-    colnames(coding.info) <- c("id","name")
-    
-    lnc.info <- cbind(row.names(lnc),data.frame(lnc[,1]) )
-    colnames(lnc.info) <- c("id","name")
-    
-    return(list(correlation=correlation, coding.info=coding.info, lnc.info=lnc.info))
-    
-  } else {
-    return("can not open the file!")
+getnetwork <- function(lnc.info, coding.info, network, lnc.name) {
+  j <- which(lnc.info$name == lnc.name)
+  lnc.id <- as.character(lnc.info[j,1]) 
+  
+  tmp <- subset(network, (Regulator == lnc.id) | (Target == lnc.id) )
+  tmp <- data.frame(tmp)
+  
+  tmp.name <- matrix(nrow = nrow(tmp), ncol = ncol(tmp))
+  for (i in 1: nrow(tmp)) {
+    for (j in 1:2) {
+      k <- which(lnc.info$id == as.character(tmp[i,j]) )
+      if (length(k)>0) {
+        tmp.name[i,j] <- as.character(lnc.info[k,2]) 
+      } else {
+        k <- which(coding.info$id == as.character(tmp[i,j]) )
+        tmp.name[i,j] <- as.character(coding.info[k,2]) 
+      }
+    }
   }
+  tmp.name[,3] <- tmp[,3]
+  tmp.name[,4] <- tmp[,4]
+  colnames(tmp.name) <- colnames(network)
+  
+  lnc.coding <- tmp.name
+  for ( i in 1: nrow(lnc.coding)){
+    k1 <- which(lnc.info$name == as.character(lnc.coding[i,1]) )
+    if (length(k1)>0){
+      k2 <- which(lnc.info$name == as.character(lnc.coding[i,2]) )
+      if (length(k2) >0 ){
+        lnc.coding[i,3] <- "-"
+      }
+    }
+  }
+  od <- data.frame(order(lnc.coding[,3],decreasing = T))
+  lnc.coding <-  lnc.coding[od[,1],]
+  lnc.coding <-  subset(lnc.coding, lnc.coding[,3] != "-")
+  
+  return(lnc.coding)
 }
 
 
-functionalPrediction <- function(lnc.name, file_correlation, file_lnc.info) {
-  if ( (file.exists(file_correlation) == TRUE) &(file.exists(file_lnc.info) == TRUE)) {
-    correlation <- fread(file_correlation,sep="\t")
-    lnc.info <- read.table(file_lnc.info, sep = "\t", head=T, row.names=1)
-    
-    j <- which(lnc.info$name == lnc.name)
-    if (length(j) > 0) {
-      k <- j+2
-      od <- data.frame(order(correlation[,..k],decreasing = T))
-      correlation.top <- correlation[od[1:30,],c(2,..k)]
-      gene <- data.frame(correlation.top[,1])
-      
-      go <- gprofiler(gene, organism = "hsapiens",ordered_query = T)
-      go <- go[order(go[,3]),]
-      
-      BP <- subset(go, domain == "BP")
-      CC <- subset(go, domain == "CC")
-      MF <- subset(go, domain == "MF")
-      
-      return(list(GO=go, BP=BP, CC=CC, MF=MF))
-    } else {
-      return("can not find the lncRNA")
-    }
-    
+functionalPrediction <- function(lnc.name, lnc.coding, gotype="all") {
+  library(gProfileR)
+  
+  gene.regulator <- setdiff(lnc.coding[,1], lnc.name)
+  gene.target <- setdiff(lnc.coding[,2], lnc.name)
+  gene.all <- setdiff(union(lnc.coding[,1], lnc.coding[,2]), lnc.name)
+  
+  if (gotype == "regulator") {
+    gene.use <- gene.regulator
+  } else if (gotype == "target") {
+    gene.use <- "target"
   } else {
-    return("can not open files!")
+    gene.use <- gene.all
   }
+  
+  GO <- gprofiler(gene.use, organism = "hsapiens",ordered_query = T,hier_filtering="strong")
+  GO <- GO[order(GO[,3]),]
+  
+  BP <- subset(GO, domain == "BP")
+  CC <- subset(GO, domain == "CC")
+  MF <- subset(GO, domain == "MF")
+  
+  return(list(gene.use=gene.use, GO=GO, BP=BP, CC=CC, MF=MF))
 }
 
